b8c7b2b3ae423fd2a0f1c1d9bab1f6e754888cf8
project2479 <jaywrsnp6@gmail.com>
2024-10-03 20:03:53 +0800

    new

--- src/components/chatbot/Header.jsx
+++ src/components/chatbot/Header.jsx
@@ -4,7 +4,7 @@ import CancelIcon from "@mui/icons-material/Cancel";
 function Header({ setIsOpen }) {
 	return (
 		<div className="sticky top-0 border-b-2 w-[350px] flex flex-row p-4 sm:p-6 shadow-lg bg-green-50/90 backdrop-blur-2xl z-50">
-			<p className="text-xl font-bold">Bryne's Assistant</p>
+			<p className="text-xl font-bold">Chatbot</p>
 			<CancelIcon
 				fontSize="medium"
 				className="cursor-pointer ml-auto text-gray-700" // Optional: Add spacing and cursor pointer
