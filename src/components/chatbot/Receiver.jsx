0eb55578db35325a26598faa87e872780ee6972b
project2479 <jaywrsnp6@gmail.com>
2024-10-11 10:50:24 +0800

    new

--- src/components/chatbot/Receiver.jsx
+++ src/components/chatbot/Receiver.jsx
@@ -1,12 +1,16 @@
-/* eslint-disable react/prop-types */ /* eslint-disable react/no-unescaped-entities */ import SmartToyIcon from "@mui/icons-material/SmartToy";function Receiver({ botResponse, timeSent }) {	return (
+/* eslint-disable react/prop-types *//* eslint-disable react/no-unescaped-entities */
+import SmartToyIcon from "@mui/icons-material/SmartToy";
+
+function Receiver({ botResponse, timeSent }) {
+	return (
 		<>
 			<div className="flex items-end gap-2 mb-3 ml-4">
-				{/* <span className="flex size-8 items-center justify-center overflow-hidden rounded-full border border-purple-300 text-sm font-bold tracking-wider  bg-purple-900 text-purple-300  break-words">
+				{/* <span className="flex size-8 items-center justify-center overflow-hidden rounded-full border border-purple-300 text-sm font-bold tracking-wider bg-purple-900 text-purple-300 break-words">
 					<SmartToyIcon className="text-white" />
 				</span> */}
 				<div className="mr-auto flex max-w-[95%] flex-col gap-2 p-4 bg-green-600 text-purple-300">
 					<span className="font-semibold text-white">Chatbot -</span>
-					<div className="text-sm text-white">"{botResponse}"</div>
+					<pre className="text-sm text-white">{botResponse}</pre>
 					{/* <span className="ml-auto text-xs text-white">{timeSent}</span> */}
 				</div>
 			</div>
