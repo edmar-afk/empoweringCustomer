58924545e1591e6b8bedfdae9887b8e756403c06
project2479 <jaywrsnp6@gmail.com>
2024-11-04 15:50:26 +0800

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,45 +1,54 @@
-/* eslint-disable react/no-unescaped-entities */import chatbotIcon from '../assets/svg/chatbot.svg'
+import { useState } from "react";
+import chatbotIcon from "../assets/svg/chatbot.svg";
+import Chatbot from "./Chatbot";
 
-function Footer() {	return (
-	<>
-		<footer>
-			<div className="bg-[#182b50] px-8 py-16">
-				<div className="max-w-6xl mx-auto grid md:grid-cols-2 justify-center items-center gap-12">
-					<div className="text-center md:text-left">
-						<h2 className="text-4xl lg:text-5xl font-extrabold text-white mb-6 md:!leading-[55px]">
-							Are you confused or need more Info?
-						</h2>
-						<p className="text-lg lg:text-xl text-white">
-							Contact our chatbot service and ask anything relevant
-						</p>
-						<a
-							href="#"
-							className="mt-12 bg-green-500 hover:bg-opacity-80 text-gray-100 py-3 px-6 rounded-full text-lg lg:text-xl transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl inline-block">
-							Chat me now!
-						</a>
-					</div>
-					<div className="text-center">
-						<img
-							src={chatbotIcon}
-							alt="Premium Benefits"
-							className="w-full mx-auto"
-						/>
-					</div>
-				</div>
-			</div>
-			<div className="bg-green-700 py-4 text-gray-100">
-				<div className="container mx-auto px-4">
-					<div className="-mx-4 flex flex-wrap justify-between">
-						<div className="px-4 w-full text-center sm:w-auto sm:text-left">
-							Empowering Customer Interactions
+function Footer() {
+	const [isChatBotOpen, setChatBotOpen] = useState(false);
+
+	return (
+		<>
+			<footer>
+				<div className="bg-[#182b50] px-8 py-16">
+					<div className="max-w-6xl mx-auto grid md:grid-cols-2 justify-center items-center gap-12">
+						<div className="text-center md:text-left">
+							<h2 className="text-4xl lg:text-5xl font-extrabold text-white mb-6 md:!leading-[55px]">
+								Are you confused or need more Info?
+							</h2>
+							<p className="text-lg lg:text-xl text-white">Contact our chatbot service and ask anything relevant</p>
+							<a
+								href="#"
+								className="mt-12 bg-green-500 hover:bg-opacity-80 text-gray-100 py-3 px-6 rounded-full text-lg lg:text-xl transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl inline-block"
+								onClick={() => setChatBotOpen(true)}>
+								Chat me now!
+							</a>
 						</div>
-						<div className="px-4 w-full text-center sm:w-auto sm:text-left">Smart Chatbots in Support Services</div>
+						<div className="text-center">
+							<img
+								src={chatbotIcon}
+								alt="Premium Benefits"
+								className="w-full mx-auto"
+							/>
+						</div>
 					</div>
 				</div>
-			</div>
-		</footer>
-	</>
-);
+				<div className="bg-green-700 py-4 text-gray-100">
+					<div className="container mx-auto px-4">
+						<div className="-mx-4 flex flex-wrap justify-between">
+							<div className="px-4 w-full text-center sm:w-auto sm:text-left">Empowering Customer Interactions</div>
+							<div className="px-4 w-full text-center sm:w-auto sm:text-left">Smart Chatbots in Support Services</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+
+			{isChatBotOpen && (
+				<Chatbot
+					isChatBotOpen={isChatBotOpen}
+					setIsOpen={setChatBotOpen}
+				/>
+			)}
+		</>
+	);
 }
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,10 @@
-function Navbar() {	return (
+import { useState } from "react";
+import Chatbot from "./Chatbot";
+
+function Navbar() {
+	const [isChatBotOpen, setChatBotOpen] = useState(false);
+
+	return (
 		<>
 			<header className="fixed top-0 left-0 py-6 flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm z-50">
 				<nav className="max-w-[85rem] w-full mx-auto px-4 flex flex-wrap basis-full items-center justify-between">
@@ -10,12 +16,19 @@ function Navbar() {	return (
 					<div className="sm:order-3 flex items-center gap-x-2">
 						<button
 							type="button"
-							className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-green-400 bg-white text-green-800 shadow-sm hover:bg-green-50 focus:outline-none focus:bg-green-50 disabled:opacity-50 disabled:pointer-events-none">
+							className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-green-400 bg-white text-green-800 shadow-sm hover:bg-green-50 focus:outline-none focus:bg-green-50 disabled:opacity-50 disabled:pointer-events-none"
+							onClick={() => setChatBotOpen(!isChatBotOpen)}>
 							Ask Question
 						</button>
 					</div>
 				</nav>
 			</header>
+			{isChatBotOpen && (
+				<Chatbot
+					isChatBotOpen={isChatBotOpen}
+					setIsOpen={setChatBotOpen}
+				/>
+			)}
 		</>
 	);
 }
